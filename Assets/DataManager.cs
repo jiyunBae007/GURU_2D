5bff482751931715e5f79e9da0fbe9cc33319582
BaejiYun <331lucy@naver.com>
2020-08-17 02:58:57 +0900

    프로젝트1,2-1장 업로드

---
+++ Assets/DataManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public float playTimeCurrent = 10f;
+    public float playTimeMax = 10f;
+
+    static DataManager instance;
+    public static DataManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            DestroyObject(gameObject);
+        }
+    }
+
+    public int score = 0;
+
+    public bool PlayerDie = false;
+}
